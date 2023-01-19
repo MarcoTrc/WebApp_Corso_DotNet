1267320c9f63405709fde84dff6d96161fe24273
MarcoTrc <marcoturchiuli@gmail.com>
2023-01-19 15:23:56 +0100

    Aggiornata l'applicazione a ASP:NET Core 2.2

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace webApp
@@ -15,7 +16,7 @@ namespace webApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +28,6 @@ namespace webApp
             }
 
             app.UseStaticFiles();
-
-            // app.Run(async (context) =>
-            // {
-            //     await context.Response.WriteAsync($"Hello DotNET!");
-            // });
 
             // app.Run(async (context) =>
             // {
